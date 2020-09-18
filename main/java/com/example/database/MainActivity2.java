d97a59022ad541a13925365e04c5273a1df968e2
mrahimian <58389747+mrahimian@users.noreply.github.com>
2020-09-18 19:30:50 +0430

    Update MainActivity2.java

--- main/java/com/example/database/MainActivity2.java
+++ main/java/com/example/database/MainActivity2.java
@@ -99,9 +99,9 @@ public class MainActivity2 extends AppCompatActivity {
 
 
             String _user = "sa";
-            String _pass = "hamdi@0912";
-            String _DB = "yadak";
-            String _server = "176.9.199.181";
+            String _pass = "----";
+            String _DB = "----";
+            String _server = "----";
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
                     .permitAll().build();
             StrictMode.setThreadPolicy(policy);
